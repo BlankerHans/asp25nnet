df7d012b0afdd6205a0d9dca3389cb15290c26b2
Florian Penzkofer <florian.penzkofer@stud.uni-goettingen.de>
2025-06-27 16:11:54 +0200

    changed neg_log_lik file

--- R/neg_log_lik.R
+++ R/neg_log_lik.R
@@ -29,3 +29,19 @@
 #' neg_log_lik(y, mu, log_sigma, reduction = "mean")
 #'
 #' @export
+neg_log_lik <- function(y, mu, log_sigma, reduction = c("sum","mean","raw")) {
+
+  reduction <- match.arg(reduction)
+
+  sigma <- exp(log_sigma)
+
+  loss_i <- log_sigma + (y - mu)^2 / (2 * sigma^2)
+
+  if (reduction == "sum") {
+    return(sum(loss_i))
+  } else if (reduction == "mean") {
+    return(mean(loss_i))
+  } else {  # reduction == "raw"
+    return(loss_i)
+  }
+}
