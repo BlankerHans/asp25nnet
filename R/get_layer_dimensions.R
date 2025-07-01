7a4f80b5d86ae4f650cae6bb25d7f693672d4cd4
Florian Penzkofer <florian.penzkofer@stud.uni-goettingen.de>
2025-07-01 12:56:20 +0200

    added Forward Pass, get Layer Dimensions & random split (with documentation) to package

---
+++ R/get_layer_dimensions.R
@@ -0,0 +1,32 @@
+#' Get Layer Dimensions for a Neural Network
+#'
+#' Determines the input, hidden, and output layer dimensions based on the input matrix
+#' and specified configuration.
+#'
+#' @param X A numeric matrix or vector representing input data. If a matrix, rows correspond to features and columns to observations.
+#' @param out_dim Integer specifying the output dimension (e.g., number of output units).
+#' @param hidden_neurons Integer specifying the number of neurons in the hidden layer.
+#' @param train Logical value indicating whether the data is used for training (currently not affecting the output). Default is \code{TRUE}.
+#'
+#' @return A named list with elements:
+#' \item{n_x}{Number of input features (rows of \code{X}).}
+#' \item{n_h}{Number of hidden neurons.}
+#' \item{n_y}{Number of output neurons.}
+#'
+#' @examples
+#' X <- matrix(rnorm(20), nrow = 5)
+#' dims <- getLayerDimensions(X, out_dim = 2, hidden_neurons = 4)
+#' dims$n_x
+#'
+#' @export
+getLayerDimensions <- function(X, out_dim, hidden_neurons, train=TRUE) {
+  n_x <- dim(X)[1]
+  n_h <- hidden_neurons
+  n_y <- out_dim
+
+  dimensions_list <- list("n_x" = n_x,
+                          "n_h" = n_h,
+                          "n_y" = n_y)
+
+  return(dimensions_list)
+}
